e8c097486f3072cfe0599adf846452b682c63bd7
gcancho <giovannicb19@gmail.com>
2020-06-13 19:54:54 -0500

    Slider de 4 imagenes

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,48 @@
-<<<<<<< HEAD
+//almacenar slider en una variable
+var slider = $('#slider');
+//almacenar botones
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
 
-    // $(".menu ul li a").css("color", "#aa55bb");
-=======
-$(document).ready(function () {
-    $(".menu ul li a").css("color", "#22aa66");
+//mover ultima imagen al primer lugar
+$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+//mostrar la primera imagen con un margen de -100%
+slider.css('margin-left', '-'+100+'%');
+
+function moverD() {
+	slider.animate({
+		marginLeft:'-'+200+'%'
+	} ,700, function(){
+		$('#slider .slider__section:first').insertAfter('#slider .slider__section:last');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function moverI() {
+	slider.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function autoplay() {
+	interval = setInterval(function(){
+		moverD();
+	}, 5000);
+}
+siguiente.on('click',function() {
+	moverD();
+	clearInterval(interval);
+	autoplay();
 });
->>>>>>> 583ba96bca3d5953b9eed06e31cb10cbc1041fc7
+
+anterior.on('click',function() {
+	moverI();
+	clearInterval(interval);
+	autoplay();
+});
+
+
+autoplay();
