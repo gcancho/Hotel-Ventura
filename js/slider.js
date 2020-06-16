514d9ceea510673016f6506c09ffbfc1125281ac
gcancho <giovannicb19@gmail.com>
2020-06-16 01:02:28 -0500

    Diseñando el login, aun falta completar y hacer el modal

--- js/slider.js
+++ js/slider.js
@@ -46,3 +46,13 @@ anterior.on('click',function() {
 
 
 autoplay();
+
+//MODAL
+$(document).on("ready", function () {
+    $("#mostrar-modal").on("click", function () {
+        $("#modal").addClass("mostrar-modal");
+    });
+    $("#cerrar-modal").on("click", function () {
+        $("#modal").removeClass("mostrar-modal");
+    });
+})
