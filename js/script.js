0ef283e36071e6a9275948501ff27c409e7ee523
gcancho <giovannicb19@gmail.com>
2020-06-16 12:01:10 -0500

    login enlazado javascript con click

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function mostrarLogin(){
+    $("#login").css("top","50%");
+    $("#login").css("left","50%");
+    $("#login").css("transform"," translate(-50%, -50%);");
+    $("#login").css("transition","all 0.8s")
+}
