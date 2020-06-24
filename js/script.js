fd4d9009b7939f6f85797cceb1d21ca08ccbefbb
gcancho <giovannicb19@gmail.com>
2020-06-23 19:27:52 -0500

    Cambio de estilos en el login

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-function mostrarLogin(){
-    $("#login").css("top","50%");
-    $("#login").css("left","50%");
-    $("#login").css("transform"," translate(-50%, -50%);");
-    $("#login").css("transition","all 0.8s")
-}
+$('.ic_login').click(function () {
+    $("#login").css("top", "50%");
+    $("#login").css("left", "50%");
+    $("#login").css("transform", " translate(-50%, -50%);");
+    $("#login").css("transition", "all 0.8s");
+    $("header").css("opacity", "0.7");
+    $("section").css("opacity", "0.7");
+    $("footer").css("opacity", "0.7");    
+});
