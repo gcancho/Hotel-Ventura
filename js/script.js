cdd3cc94106f78050eab6921d3a412feeea88359
gcancho <giovannicb19@gmail.com>
2020-06-28 13:42:57 -0500

    Avance de vista de contacto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$('.ic_login').click(function () {
-    $("#login").css("top", "50%");
-    $("#login").css("left", "50%");
-    $("#login").css("transform", " translate(-50%, -50%);");
-    $("#login").css("transition", "all 0.8s");
-    $("header").css("opacity", "0.7");
-    $("section").css("opacity", "0.7");
-    $("footer").css("opacity", "0.7");    
-});
+ $('.ic_login').click(function () {
+     $(".login").css("top", "50%");
+     $(".login").css("left", "50%");
+     $(".login").css("transform", " translate(-50%, -50%);");
+     $(".login").css("transition", "all 0.8s");
+     $("header").css("opacity", "0.7");
+     $("section").css("opacity", "0.7");
+     $("footer").css("opacity", "0.7");    
+ });
